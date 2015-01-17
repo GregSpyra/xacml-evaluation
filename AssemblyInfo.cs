504174e6020236691d8f1e583d9aea618f88ffcf
gregor.spyra@gmail.com <gregor.spyra@gmail.com@c7274a0d-7975-716f-5236-9cbdeeedd94f>
2015-01-17 17:04:31 +0000

    CREATE: G$; initial

---
+++ AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyKeyName("")]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("XACML Implementation for .Net")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Test Client")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: PermissionSet(SecurityAction.RequestMinimum, XML = "<PermissionSet class=\"System.Security.PermissionSet\"\r\n               version=\"1\">\r\n   <IPermission class=\"System.Security.Permissions.SecurityPermission, mscorlib, Version=1.0.5000.0, Culture=neutral, PublicKeyToken=b77a5c561934e089\"\r\n                version=\"1\"\r\n                Flags=\"Assertion, Execution\"/>\r\n</PermissionSet>\r\n")]
